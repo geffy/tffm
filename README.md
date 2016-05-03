7ec8cc0883d1eb2c52afa4a8a0ab5d102500d2bc
Mikhail Trofimov <mikhail.trofimov@phystech.edu>
2016-05-03 16:58:00 +0300

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+This is a TensorFlow implementation of Factorization Machine based on paper [Factorization Machines with libFM](http://dl.acm.org/citation.cfm?doid=2168752.2168771). It supports `d`-order FM, where `d>=2`
+
+
+# Prerequisites
+* [Scikit-learn](http://scikit-learn.org/stable/)
+* [Numpy](http://www.numpy.org/)
+* [TensorFlow 0.8](https://www.tensorflow.org/)
+
+# Usage
+The interface is the same as of Scikit-learn models. To train a 6-order FM model with rank=10 for 100 iterations with learning_rate=0.01 use the following sample
+```
+model = tffm.TFFMClassifier(interactions=6, rank=10, n_epochs=100,  lr=0.01)
+model.fit(X_train, y_train)
+```
---
+++ tffm.py
@@ -0,0 +1,151 @@
+"""A tensorflow implementation of d-way FM (d>=2)
+    Should support sklearn stuff like cross-validation.
+"""
+from sklearn.linear_model.base import BaseEstimator, LinearClassifierMixin
+import sklearn
+import numpy as np
+import tensorflow as tf
+
+
+class TFFMClassifier(BaseEstimator, LinearClassifierMixin):
+    def __init__(self, rank, interactions=2, fit_intercept=True, lr=0.1, batch_size=-1, reg=0, init_std=0.01, n_epochs=100, verbose=0):
+
+        # Save all the params as class attributes. It's required by the
+        # BaseEstimator class.
+        self.rank = rank
+        self.lr = lr
+        self.batch_size = batch_size
+        self.fit_intercept = fit_intercept
+        self.reg = reg
+        self.interactions = interactions
+        self.n_epochs = n_epochs
+        self.verbose = verbose
+        self.init_std = init_std
+        self.graph = None
+        self.nfeats = None
+
+    def initialize_graph(self):
+        assert self.nfeats is not  None
+
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+
+            with tf.name_scope('params') as scope: 
+                self.w = [None] * self.interactions
+                for i in range(1, self.interactions+1):
+                    r = self.rank
+                    if i == 1:
+                        r = 1
+                    self.w[i-1] = tf.Variable(tf.random_uniform([self.nfeats, r], -self.init_std, self.init_std),
+                                              trainable=True,
+                                              name='embedding_'+str(i))
+
+                self.b = tf.Variable(
+                    self.init_std,
+                    trainable = True,
+                    name='bias')
+
+            with tf.name_scope('inputBlock') as scope:
+                self.train_x = tf.placeholder(tf.float32, shape=[None, self.nfeats], name='x')
+                self.train_y = tf.placeholder(tf.float32, shape=[None], name='Y')
+
+            with tf.name_scope('mainBlock') as scope:
+                self.outputs = tf.matmul(self.train_x, self.w[0])
+                for i in range(2, self.interactions+1):
+                    dot = tf.pow(tf.matmul(self.train_x, self.w[i-1]), i)
+                    # Subtract diagonal elements.
+                    dot -= tf.matmul(tf.pow(self.train_x, i), tf.pow(self.w[i-1], i))
+                    self.outputs += tf.reshape(tf.reduce_sum(dot, [1]), [-1, 1])
+                self.outputs += self.b
+
+                self.probs = tf.sigmoid(self.outputs, name='probs')
+                self.loss = tf.minimum(
+                    tf.log(tf.add(1.0, tf.exp(-self.train_y * tf.transpose(self.outputs)))), 
+                    100, name='truncated_log_loss') 
+
+                self.regularization = 0
+                for i in range(1, self.interactions+1):
+                    self.regularization += tf.nn.l2_loss(self.w[i-1], name='regularization_penalty_'+str(i))
+
+            self.target = tf.reduce_mean(self.loss) + self.reg*self.regularization
+            self.optimizer = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(self.target)
+
+    def initialize_session(self):
+        # TODO: get rid of interactive session.
+        self.session = tf.InteractiveSession(graph=self.graph)
+        init = tf.initialize_all_variables()
+        self.session.run(init)
+        # TODO: add SummaryWriter
+
+    def destroy(self):
+        self.session.close()
+        self.graph = None
+
+    def batcher(self, X_, y_=None):
+        if self.batch_size == -1:
+            batch_size = X_.shape[0]
+        else:
+            batch_size = self.batch_size
+
+        for i in range(0, X_.shape[0], batch_size):
+            retX = X_[i:i+batch_size]
+            retY = None
+            if y_ is not None:
+                retY = y_[i:i+batch_size]
+
+            yield (retX, retY)
+
+    def fit(self, X_, y_, n_epochs=None):
+        self.nfeats = X_.shape[1]
+        assert self.nfeats is not None
+        if self.graph is None:
+            self.initialize_graph()
+            self.initialize_session()
+        used_y = y_*2 - 1 # suppose input {0, 1}, but use instead {-1, 1} labels
+
+        if n_epochs is None:
+            n_epochs = self.n_epochs
+        # Training cycle
+        for epoch in range(n_epochs):
+            if self.verbose>0:
+                print 'start epoch: {}'.format(epoch)
+
+            # generate permutation
+            perm = np.random.permutation(X_.shape[0])
+
+            # iterate over batches
+            for i, (bX, bY) in enumerate(self.batcher(X_[perm], used_y[perm])):
+                fd = {
+                    self.train_x : bX.astype(np.float32), 
+                    self.train_y : bY.astype(np.float32),
+                }
+                self.session.run(self.optimizer, feed_dict=fd)
+
+                if self.verbose > 1:
+                    batch_target_value = self.session.run(self.target, feed_dict=fd)
+                    w_sum = self.regularization.eval()
+                    print ' -> batch: {}, target: {}, w_sum: {}'.format(i, batch_target_value, w_sum)
+
+    def decision_function(self, X):
+        if self.graph is None:
+            raise sklearn.exceptions.NotFittedError("Call fit before prediction")
+        output = []
+        for (bX, bY) in self.batcher(X):
+            fd = {
+                self.train_x : bX.astype(np.float32), 
+            }
+            output.append(self.session.run(self.outputs, feed_dict=fd))
+        # TODO: reshape
+        return np.concatenate(output)
+
+    def predict_proba(self, X):
+        if self.graph is None:
+            raise sklearn.exceptions.NotFittedError("Call fit before prediction")
+        output = []
+        for (bX, bY) in self.batcher(X):
+            fd = {
+                self.train_x : bX.astype(np.float32), 
+            }
+            output.append(self.session.run(self.probs, feed_dict=fd))
+        # TODO: reshape
+        return np.concatenate(output)
